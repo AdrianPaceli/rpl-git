0e006052299237d53c7585c72172e6d421118ff9
Adrian Paceli <32383568+AdrianPaceli@users.noreply.github.com>
2018-11-03 22:54:32 +0700

    Update Kalkulator.java

--- Kalkulator.java
+++ Kalkulator.java
@@ -1,1 +1,22 @@
-
+public class Kalkulator{
+	
+	public Kalkulator(){
+		
+	}
+	
+	public float plus(float a, float b){
+		
+	}
+	
+	public float minus(float a, float b){
+		
+	}
+	
+	public float multiply(float a, float b){
+		
+	}
+	
+	public float division(float a, float b){
+		
+	}
+}
